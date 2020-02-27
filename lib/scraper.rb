3b66ef8f2b549ca880a2abc79a423d1943ae4e4e
Kyle Talmage <ktalmage85@gmail.com>
2020-02-27 19:40:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,4 +13,4 @@ courses.each do |course|
   puts course.text.strip
 end
 
-p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].name
+p doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid2-3jXtmC")[0].attributes
